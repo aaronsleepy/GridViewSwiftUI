58b3b3dba9b031f0c0e92086cf5fc4bce3452b65
aaronsleepy <aaron@kmong.com>
2022-12-10 22:51:15 +0900

    init: first commit

---
+++ GridViewSwiftUI/GridViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewSwiftUIApp.swift
+//  GridViewSwiftUI
+//
+//  Created by Aaron on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct GridViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
