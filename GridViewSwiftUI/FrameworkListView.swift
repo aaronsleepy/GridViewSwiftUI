1dc0f74090f375346176210c33d5573d9cd9525a
aaronsleepy <aaron@kmong.com>
2022-12-11 16:56:10 +0900

    ui: LazyVGrid를 사용하여 그리드 뷰 구현

--- GridViewSwiftUI/FrameworkListView.swift
+++ GridViewSwiftUI/FrameworkListView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct FrameworkListView: View {
+    // Data
+    @State var list: [AppleFramework] = AppleFramework.list
+    
+    // Layout
+    let layout: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(list) { item in
+                    FrameworkCell(framework: item)
+                }
+            }
+            .padding([.top, .leading, .trailing], 16.0)
         }
-        .padding()
     }
 }
 
